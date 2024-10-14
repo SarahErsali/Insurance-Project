34312f4f6b1f2bca86ef2ceb1b06741b18a81def
Sara Ersali <sara.ersali@gmail.com>
2024-10-14 22:54:06 +0200

    Some interpretation is added

--- components/tabs/tab3.py
+++ components/tabs/tab3.py
@@ -9,14 +9,54 @@ import matplotlib.pyplot as plt
 
 def render_tab3():
     return html.Div([
-        html.H2("Model Performance", style={'textAlign': 'left', 'fontSize': '28px'}),
+        html.H3("Models Evaluation", style={
+            'fontWeight': 'bold', 
+            'textAlign': 'left', 
+            'fontSize': '30px', 
+            'marginTop': '3cm'  # 3 cm space to the top of the page
+        }),
+
+        html.P("The line plot shows the predictions of four different models—XGBoost, ARIMA, LightGBM, and Moving Average—against the actual values for 'Claims Incurred' over a period. The dotted black line represents the actual observed values, while the colored lines show the predictions of the models.",
+               style={
+                   'fontSize': '16px', 
+                   'textAlign': 'left', 
+                   'lineHeight': '2.0',
+                   #'marginTop': '3cm'  # Space from the top of the page
+               }),
+
+        # Bullet points for model interpretation
+        html.Ul([
+            html.Li([
+                html.Span("XGBoost: ", style={'fontWeight': 'bold'}),
+                "In the flatter regions or moderate fluctuations, XGBoost tends to smooth out the predictions but still follows the overall pattern. The deviation from actual values is relatively small."
+            ], style={'fontSize': '16px', 'textAlign': 'left', 'lineHeight': '2'}),  # Increased line spacing
+            
+            html.Li([
+                html.Span("LightGBM: ", style={'fontWeight': 'bold'}),
+                "LightGBM seems less stable and has larger prediction errors in regions with moderate variations in claims."
+            ], style={'fontSize': '16px', 'textAlign': 'left', 'lineHeight': '2'}),  # Increased line spacing
+            
+            html.Li([
+                html.Span("ARIMA: ", style={'fontWeight': 'bold'}),
+                "ARIMA performs better in flatter regions, where the actual values are relatively stable."
+            ], style={'fontSize': '16px', 'textAlign': 'left', 'lineHeight': '2'}),  # Increased line spacing
+            
+            html.Li([
+                html.Span("Moving Average: ", style={'fontWeight': 'bold'}),
+                "The Moving Average model shows a smoother pattern compared to the others, which is expected due to its nature. However, it struggles to capture sudden shifts and spikes in the data."
+            ], style={'fontSize': '16px', 'textAlign': 'left', 'lineHeight': '2'})  # Increased line spacing
+        ], style={'textAlign': 'left', 'marginBottom': '2.5cm'}),
+
+
+        #html.H2("Model Performance", style={'textAlign': 'left', 'fontSize': '28px'}),
+
 
         # Instruction line for dropdown (model selection)
         html.P("Select the models you would like to evaluate", style={
             'textAlign': 'left', 
-            'fontSize': '16px', 
-            'marginTop': '10px', 
-            'marginBottom': '15px'  # Increase space between text and dropdown
+            'fontSize': '20px', 
+            'marginTop': '4px', 
+            'marginBottom': '5px'  # Increase space between text and dropdown
         }),
 
         # Dropdown for model selection (for model predictions)
@@ -42,7 +82,34 @@ def render_tab3():
         # Graph for comparing models' predictions
         dcc.Graph(id='model-comparison-graph'),
 
-        html.H2("Model Performance Metrics", style={'textAlign': 'left', 'fontSize': '28px', 'marginTop': '50px'}),
+        html.H3("Models Performance Metrics", style={
+            'fontWeight': 'bold', 
+            'textAlign': 'left', 
+            'fontSize': '30px', 
+            'marginTop': '3cm'  # 3 cm space to the top of the page
+        }),
+        #html.H2("Model Performance ", style={'textAlign': 'left', 'fontSize': '28px', 'marginTop': '50px'}),
+        html.P("The bar chart evaluates each model based on three key metrics: Bias, Accuracy, and MAPE.",
+               style={
+                   'fontSize': '16px', 
+                   'textAlign': 'left', 
+                   'lineHeight': '2.0',
+                   #'marginTop': '3cm'  # Space from the top of the page
+               }),
+        
+        # Text explanation for the performance metrics in bullet points
+        html.Ul([
+            html.Li([
+                html.Span("ARIMA: ", style={'fontWeight': 'bold'}),
+                "ARIMA achieves moderate accuracy, lower than XGBoost but better than LightGBM and Moving Average. In addition, it has a slightly negative bias but is closer to zero than the other models, which means it provides relatively unbiased predictions on average. Moreover, the MAPE for ARIMA is also reasonable, indicating decent predictive performance."
+            ], style={'fontSize': '16px', 'textAlign': 'left', 'lineHeight': '2'}),
+
+            html.Li([
+                html.Span("LightGBM: ", style={'fontWeight': 'bold'}),
+                "LightGBM has a more substantial negative and high bias and error, suggesting it consistently underpredicts the actual values, although its accuracy value is high. It shows more volatility in predictions, making it less reliable than other models."
+            ], style={'fontSize': '16px', 'textAlign': 'left', 'lineHeight': '2'}),   
+        ], style={'textAlign': 'left', 'marginBottom': '2.5cm'}),
+
 
         # Instruction line for dropdown (metrics selection)
         html.P("Select the models for which you want to analyze performance metrics", style={
--- components/tabs/tab4.py
+++ components/tabs/tab4.py
@@ -2,7 +2,35 @@ from dash import html, dcc
 
 def render_tab4():
     return html.Div([
-        html.H2("Model Stress Testing on Storm Periods", style={'textAlign': 'left', 'fontSize': '28px'}),
+        html.H2("Models Stress Testing on Storm Periods", style={'textAlign': 'left', 'fontSize': '28px', 'marginTop': '3cm'}),
+
+        html.P("The storm period plot presents a clear difference in how the models perform during storm events.",
+               style={
+                   'fontSize': '16px', 
+                   'textAlign': 'left', 
+                   'lineHeight': '2.0',
+                   #'marginTop': '3cm'  # Space from the top of the page
+               }),
+
+        # Bullet points with bold titles
+        html.Ul([
+            html.Li([
+                html.Span("XGBoost: ", style={'fontWeight': 'bold'}),
+                "XGBoost predictions closely follow the actual claims during storm periods, with some deviations but generally capturing the trend well."
+            ], style={'fontSize': '16px', 'textAlign': 'left', 'lineHeight': '2.0'}),
+
+            html.Li([
+                html.Span("LightGBM: ", style={'fontWeight': 'bold'}),
+                "LightGBM shows larger deviations from the actual claims, particularly during the later storm periods, where it underestimates the claims significantly. This suggests that XGBoost is better at handling these extreme conditions and LightGBM struggles more with the volatility introduced by storm scenarios, resulting in more significant errors."
+            ], style={'fontSize': '16px', 'textAlign': 'left', 'lineHeight': '2.0'}),
+
+            html.Li([
+                html.Span("LightGBM (2023-05): ", style={'fontWeight': 'bold'}),
+                "The drop in predicted claims for LightGBM during the storm period (2023-05) is particularly notable, where it predicts significantly lower values than the actual observed claims."
+            ], style={'fontSize': '16px', 'textAlign': 'left', 'lineHeight': '2.0'})
+        ], style={'textAlign': 'left', 'marginBottom': '2cm'}),
+        
+        #html.H2("Model Stress Testing on Storm Periods", style={'textAlign': 'left', 'fontSize': '28px'}),
 
         # Instruction for dropdown
         html.P("Select the models you want to test during storm periods", style={
